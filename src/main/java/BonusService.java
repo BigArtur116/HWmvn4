8071036c895fc796fef4d070cac0c4828794cbcc
Artur Big <bigartur41@gmail.com>
2024-10-14 19:54:12 +0300

    Fix #1

--- src/main/java/BonusService.java
+++ src/main/java/BonusService.java
@@ -1,12 +1,11 @@
 public class BonusService {
 
     public int calculate(long amount, boolean registered) {
-        int Percent = registered ? 3 : 1;
-        long bonus = amount * Percent / 100;
+        int percent = registered ? 3 : 1;
+        long bonus = amount * percent / 100;
         long limit =500;
         if (bonus > limit) {
             bonus =limit;
         }
         return (int) bonus;
-    }
-}
+    
